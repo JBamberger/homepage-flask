b5fbb3a31f01b31051f96749668ffdd1184268ec
Jannik Bamberger <dev.jbamberger@gmail.com>
2017-12-23 02:33:49 +0100

    init

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return 'Live...'
+
+
+@app.route('/data')
+def names():
+    data = {"names":["Karl","Heinz","Jörg"]}
+    return jsonify(data)
+
+
+
+if __name__ == '__main__':
+    app.run()
+
+
