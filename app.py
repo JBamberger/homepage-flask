3a25170a8a5943d2d65ef6765ba2da7987abccae
Jannik Bamberger <dev.jbamberger@gmail.com>
2017-12-23 02:58:06 +0100

    nootnoot

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Dynamic content? content?! Fancy...'
+    return 'Dynamidffgdsfg content? content?! Fancy...'
 
 
 @app.route('/data')
