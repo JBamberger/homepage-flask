143cf59e8dee6bcde08f5eb72a502b690c6fb195
Jannik Bamberger <dev.jbamberger@gmail.com>
2017-12-24 01:52:59 +0100

    deploy test

--- app.py
+++ app.py
@@ -5,12 +5,14 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Want a flask?'
+    return 'Hello. This site has no content. Sorry.'
 
 
 @app.route('/data')
 def names():
-    data = {"names":["Noot", "Noot!"]}
+    data = {"names": ["Jannik"],
+            "numbers": [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+            "alphabet": [chr(x) for x in range(65, 65 + 26)]}
     return jsonify(data)
 
 
